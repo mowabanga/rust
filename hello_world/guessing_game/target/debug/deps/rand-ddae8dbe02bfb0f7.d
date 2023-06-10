151e0c9381fd1d9e87c8c9e6efd30418bba13f8f
mowabanga <itsmundia@gmail.com>
2023-06-10 23:16:05 +0300

    Why rust?

---
+++ hello_world/guessing_game/target/debug/deps/rand-ddae8dbe02bfb0f7.d
@@ -0,0 +1,9 @@
+/home/collins/Desktop/rust/hello_world/guessing_game/target/debug/deps/rand-ddae8dbe02bfb0f7.rmeta: /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/collins/Desktop/rust/hello_world/guessing_game/target/debug/deps/librand-ddae8dbe02bfb0f7.rlib: /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/collins/Desktop/rust/hello_world/guessing_game/target/debug/deps/rand-ddae8dbe02bfb0f7.d: /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
+/home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
+/home/collins/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
